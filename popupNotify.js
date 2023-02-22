60c4933f2b957b12c5d615861e4a39b481aae478
wedad <wedadalqithmi@gmail.com>
2023-02-22 07:21:45 +0300

    Changes to be committed: modified:   index.html modified:   popupNotify.js

--- popupNotify.js
+++ popupNotify.js
@@ -1,4 +1,22 @@
+const template = document.createElement('template');
+    template.innerHTML = ' 
+        <style>
+
+        </style>
+        
+        <div class="tooltip-container">
+            test 
+        </div>
+';
 
 
 
-class PopupNotify extends html
+class PopupNotify extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode:'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+window.customElements.define('popup-notify',PopupNotify)
